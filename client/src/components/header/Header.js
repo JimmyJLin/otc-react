c8d95e176c287e6c232a61838e9cf27396a53989
Jimmy Lin <usmcjimmy@gmail.com>
2017-09-25 15:04:19 -0400

    changed navbar collaped css

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -9,49 +9,83 @@ class Header extends Component {
   render() {
     return (
       <div>
-        <Navbar>
+        {/* <Navbar inverse collapseOnSelect>
+
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">React-Bootstrap</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+
+          <Navbar.Collapse>
+            <Nav>
+              <NavItem eventKey={1} href="#">Link</NavItem>
+              <NavItem eventKey={2} href="#">Link</NavItem>
+              <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                <MenuItem eventKey={3.1}>Action</MenuItem>
+                <MenuItem eventKey={3.2}>Another action</MenuItem>
+                <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey={3.3}>Separated link</MenuItem>
+              </NavDropdown>
+            </Nav>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="#">Link Right</NavItem>
+              <NavItem eventKey={2} href="#">Link Right</NavItem>
+            </Nav>
+          </Navbar.Collapse>
+
+        </Navbar> */}
+
+        <Navbar collapseOnSelect>
+
           <Navbar.Header>
             <Navbar.Brand>
               <Link to={'/'}>
                 <img style={{width: '50%'}} src={logo} alt=""/>
               </Link>
             </Navbar.Brand>
+            <Navbar.Toggle />
           </Navbar.Header>
-          <Nav>
-            <NavDropdown eventKey={2} title="Seminars" id="basic-nav-dropdown">
-              <LinkContainer to={'/seminars/hair_strokes'}>
-                <MenuItem eventKey={2.1}>Hair Strokes</MenuItem>
-              </LinkContainer>
-              <LinkContainer to={'/seminars/powdered'}>
-                <MenuItem eventKey={2.2}>Powdered</MenuItem>
-              </LinkContainer>
-              <LinkContainer to={'/seminars/lips'}>
-                <MenuItem eventKey={2.3}>Lips</MenuItem>
-              </LinkContainer>
-              <LinkContainer to={'/seminars/eyeliners'}>
-                <MenuItem eventKey={2.4}>Eyeliners</MenuItem>
-              </LinkContainer>
-              <LinkContainer to={'/seminars/areola'}>
-                <MenuItem eventKey={2.5}>Areola</MenuItem>
-              </LinkContainer>
-              <LinkContainer to={'/seminars/hairlines'}>
-                <MenuItem eventKey={2.6}>Hairlines</MenuItem>
-              </LinkContainer>
-              <LinkContainer to={'/seminars/others'}>
-                <MenuItem eventKey={2.7}>Others</MenuItem>
-              </LinkContainer>
-            </NavDropdown>
-            <LinkContainer to={'/register'}>
-              <NavItem eventKey={3}>Register</NavItem>
-            </LinkContainer>
-            <LinkContainer to={'/faq'}>
-              <NavItem eventKey={4} href="#">FAQ</NavItem>
-            </LinkContainer>
-            <LinkContainer to={'/contact'}>
-              <NavItem eventKey={5} href="#">Contact</NavItem>
-            </LinkContainer>
 
-          </Nav>
+          <Navbar.Collapse>
+            <Nav>
+              <NavDropdown eventKey={1} title="Seminars" id="basic-nav-dropdown">
+                <LinkContainer to={'/seminars/hair_strokes'}>
+                  <MenuItem eventKey={1.1}>Hair Strokes</MenuItem>
+                </LinkContainer>
+                <LinkContainer to={'/seminars/powdered'}>
+                  <MenuItem eventKey={1.2}>Powdered</MenuItem>
+                </LinkContainer>
+                <LinkContainer to={'/seminars/lips'}>
+                  <MenuItem eventKey={1.3}>Lips</MenuItem>
+                </LinkContainer>
+                <LinkContainer to={'/seminars/eyeliners'}>
+                  <MenuItem eventKey={1.4}>Eyeliners</MenuItem>
+                </LinkContainer>
+                <LinkContainer to={'/seminars/areola'}>
+                  <MenuItem eventKey={1.5}>Areola</MenuItem>
+                </LinkContainer>
+                <LinkContainer to={'/seminars/hairlines'}>
+                  <MenuItem eventKey={1.6}>Hairlines</MenuItem>
+                </LinkContainer>
+                <LinkContainer to={'/seminars/others'}>
+                  <MenuItem eventKey={1.7}>Others</MenuItem>
+                </LinkContainer>
+              </NavDropdown>
+              <LinkContainer to={'/register'}>
+                <NavItem eventKey={2}>Register</NavItem>
+              </LinkContainer>
+              <LinkContainer to={'/faq'}>
+                <NavItem eventKey={3} href="#">FAQ</NavItem>
+              </LinkContainer>
+              <LinkContainer to={'/contact'}>
+                <NavItem eventKey={4} href="#">Contact</NavItem>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+
         </Navbar>
       </div>
     );
