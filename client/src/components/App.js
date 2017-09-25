e7eb5d4b5910679f8e6678f0531b4baddb53ed9f
Jimmy Lin <usmcjimmy@gmail.com>
2017-09-25 10:44:34 -0400

    added react redux

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>Hello World!</div>
+    );
+  }
+}
+
+export default App;
