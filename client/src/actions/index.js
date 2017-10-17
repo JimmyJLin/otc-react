4ace68f7f004620ab20b62eba01fbc840ca079c2
Jimmy Lin <usmcjimmy@gmail.com>
2017-10-17 14:17:09 -0400

    uncommoned out post

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,7 +3,7 @@ import { reset } from 'redux-form';
 
 export const submitModel = (values, history) => async dispatch => {
   // console.log('values inside action', values)
-  // const res = await axios.post('/api/registration/models', values);
+  await axios.post('/api/registration/models', values);
 
   history.push('/');
   // dispatch({ type: 'model_registration', payload: res.data })
